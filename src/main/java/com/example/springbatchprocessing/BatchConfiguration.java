ae52d5e9b27154b655a97ac61d655636d6835897
Kotsanai Chikosi <krcchiko@gmail.com>
2024-01-31 04:09:33 -0500

    configuration details
    add config details and job listener

--- src/main/java/com/example/springbatchprocessing/BatchConfiguration.java
+++ src/main/java/com/example/springbatchprocessing/BatchConfiguration.java
@@ -1,5 +1,10 @@
 package com.example.springbatchprocessing;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
@@ -7,6 +12,7 @@ import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -14,6 +20,7 @@ import javax.sql.DataSource;
 public class BatchConfiguration {
     /**
      * reader looks for a file called sample-data.csv and parses each line item with enough information to turn it into a Person
+     *
      * @return
      */
     @Bean
@@ -22,13 +29,14 @@ public class BatchConfiguration {
                 .name("personItemReader")
                 .resource(new ClassPathResource("sample-data.csv"))
                 .delimited()
-                .names("firstName","lastName")
+                .names("firstName", "lastName")
                 .targetType(Person.class)
                 .build();
     }
 
     /**
-     *  creates an instance of the PersonItemProcessor to convert the data to upper case.
+     * creates an instance of the PersonItemProcessor to convert the data to upper case.
+     *
      * @return
      */
     @Bean
@@ -38,15 +46,56 @@ public class BatchConfiguration {
 
     /**
      * takes in a copy of the dataSource, executes the SQL statement needed to insert a single Person, for each record.
+     *
      * @param datasource
      * @return
      */
     @Bean
-    public JdbcBatchItemWriter<Person> writer(DataSource datasource){
-        return new  JdbcBatchItemWriterBuilder<Person>()
+    public JdbcBatchItemWriter<Person> writer(DataSource datasource) {
+        return new JdbcBatchItemWriterBuilder<Person>()
                 .sql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)")
                 .dataSource(datasource)
                 .beanMapped()
+                .build();
+    }
+
+    /**
+     * define the job
+     * this job has only one step
+     *
+     * @param jobRepository
+     * @param step
+     * @param listener
+     * @return
+     */
+    @Bean
+    public Job importPersonJob(JobRepository jobRepository, Step step, JobCompletionNotificationListener listener) {
+        return new JobBuilder("importPersonJob", jobRepository)
+                .listener(listener)
+                .start(step)
+                .build();
+    }
+
+    /**
+     * define the steps
+     * in the step definition, you can define how much data to write at a time.
+     * in this case, it writes up to three records at a time.
+     *
+     * @param jobRepository
+     * @param transactionManager
+     * @param reader
+     * @param processor
+     * @param writer
+     * @return
+     */
+    @Bean
+    public Step step(JobRepository jobRepository, DataSourceTransactionManager transactionManager,
+                     FlatFileItemReader<Person> reader, PersonItemProcessor processor, JdbcBatchItemWriter<Person> writer) {
+        return new StepBuilder("step", jobRepository)
+                .<Person, Person>chunk(3, transactionManager)
+                .reader(reader)
+                .processor(processor)
+                .writer(writer)
                 .build();
     }
 }
